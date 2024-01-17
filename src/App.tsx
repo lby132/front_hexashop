5dcbc5b62ef9c669bc5e9ad1436a4412858d244b
lby132 <quddbscjswow@naver.com>
2024-01-17 18:58:32 +0900

    프론트 첫화면

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Head from "./components/header/Head";
+import Footer from "./components/footer/Footer";
+import MainPage from "./pages/main/MainPage";
+import './components/css/bootstrap.min.css';
+import './components/css/flex-slider.css';
+import './components/css/font-awesome.css';
+import './components/css/lightbox.css';
+import Header from "./components/header/Header";
+// import './components/css/owl-carousel.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Head/>
+        <Header/>
+        <MainPage/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/header/Head.tsx
@@ -0,0 +1,31 @@
+
+const Head = () => {
+    return (
+        <>
+            <meta charSet="utf-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+            <meta name="description" content=""/>
+            <meta name="author" content=""/>
+            <link
+                href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap"
+                rel="stylesheet"/>
+
+            <title>Hexashop 11Ecommerce HTML CSS Template</title>
+
+
+            <link rel="stylesheet" type="text/css" href="components/css/bootstrap.min.css"/>
+
+            <link rel="stylesheet" type="text/css" href="components/css/font-awesome.css"/>
+
+            <link rel="stylesheet" href="components/css/templatemo-hexashop.css"/>
+
+            <link rel="stylesheet" href="components/css/owl-carousel.css"/>
+
+            <link rel="stylesheet" href="components/css/lightbox.css"/>
+
+        </>
+
+    );
+}
+
+export default Head;
