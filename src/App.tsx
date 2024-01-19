f4e93ac9b24f3e57de28e8d46c45dd087ae00bde
lby132 <quddbscjswow@naver.com>
2024-01-19 18:51:49 +0900

    라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Head from "./components/header/Head";
+import Footer from "./components/footer/Footer";
+import MainPage from "./pages/MainPage";
+import './components/css/bootstrap.min.css';
+import './components/css/flex-slider.css';
+import './components/css/font-awesome.css';
+import './components/css/lightbox.css';
+import Header from "./components/header/Header";
+import {BrowserRouter, Route, Router, Routes} from "react-router-dom";
+import About from "./pages/about/About";
+
+// import './components/css/owl-carousel.css';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+                    <Head/>
+                    <Header/>
+            <Routes>
+                <Route path="about" element={<About/>}/>
+            </Routes>
+                    <MainPage/>
+                    <Footer/>
+        </div>
+    );
 }
 
 export default App;
